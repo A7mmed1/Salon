eca0e044d2a6b5664ea575082a301822c89f83e5
ahmmed <ahmmedmohammed90x@hotmail.com>
2019-10-13 22:37:08 -0600

    t

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
-            https_response_code(400);
+            http_response_code(400);
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";
             exit;
         }
@@ -32,17 +32,17 @@
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
-            https_response_code(200);
+            http_response_code(200);
             echo "Thank You! Your message has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
-            https_response_code(500);
+            http_response_code(500);
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
-        https_response_code(403);
+        http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
 
